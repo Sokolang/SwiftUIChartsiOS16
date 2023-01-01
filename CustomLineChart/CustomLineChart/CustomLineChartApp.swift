4637ec4f7ce8517e8ea38e377901653caa54eabb
Anzhelika Sokolova <sokolang.developer@gmail.com>
2023-01-01 21:28:45 +0200

    Add files via upload

---
+++ CustomLineChart/CustomLineChart/CustomLineChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLineChartApp.swift
+//  CustomLineChart
+//
+//  Created by Anzhelika Sokolova on 01.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomLineChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
